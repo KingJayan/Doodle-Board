acf7d84504059a563f74be0a6f2f863e2f1c3143
Jayan P <jayanp0202@gmail.com>
2026-01-01 18:41:03 -0600

    remove mainFields

--- vite.config.ts
+++ vite.config.ts
@@ -8,9 +8,6 @@ export default defineConfig(({ mode }) => {
     plugins: [angular()],
     define: {
       'process.env.API_KEY': JSON.stringify(env.API_KEY),
-    },
-    resolve: {
-      mainFields: ['module'],
-    },
+    }
   };
 });
