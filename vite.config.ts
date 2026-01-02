91e9ae1021d50045b7e67529dca40b3ba3c56082
Jayan P <jayanp0202@gmail.com>
2026-01-02 17:15:29 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,13 @@ import { defineConfig, loadEnv } from 'vite';
 import angular from '@analogjs/vite-plugin-angular';
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, (process as any).cwd(), '');
+  const env = loadEnv(mode, process.cwd(), '');
 
   return {
     plugins: [angular()],
     define: {
       'process.env.API_KEY': JSON.stringify(env.API_KEY),
-      'process.env': {},
-      'process': {}
+      'process.env': JSON.stringify({}) 
     }
   };
 });
